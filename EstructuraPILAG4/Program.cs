c6d329d173dcd466d75c6802d5485b441db1bc9e
Armando Rodríguez <jarodriguezvera@gmail.com>
2020-12-09 14:12:33 -0600

    Agregar archivos de proyecto.

---
+++ EstructuraPILAG4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EstructuraPILAG4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<string> pila = new Stack<string>();
+            pila.Push("Hola");
+            pila.Push("soy");
+            pila.Push("Armando");
+
+            int totalElementos = pila.Count;
+
+            for(int i=1; i <= totalElementos ; i++  )
+            {
+                Console.WriteLine(pila.Pop() );
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EstructurasPilasColasG4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("El factorial de 5: {0} ", Factorial(5));
+
+            string frase = "Anita lava la tina";
+            
+
+            for(int i=0; i < frase.Length; i++)
+            {
+                Console.WriteLine(frase[i]);
+            }    
+
+        }
+        static ulong Factorial(ushort N )
+        {
+            if(N==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return N * Factorial(  (ushort)(N - 1) );
+            }
+
+        }
+    }
+}
