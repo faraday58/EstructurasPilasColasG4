bc3e8cd5fb4f2464caa2decf644d57dcaef60467
Armando Rodríguez <jarodriguezvera@gmail.com>
2020-12-09 14:14:18 -0600

    Estructuras Cola

---
+++ EstructuraColaG4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace EstructuraColaG4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> diasSemana = new Queue<string>();
+
+            diasSemana.Enqueue("Lunes");
+            diasSemana.Enqueue("Martes");
+            diasSemana.Enqueue("Miércoles");
+            diasSemana.Enqueue("Jueves");
+
+            int totaldias = diasSemana.Count;
+
+            for (int i =0; i< totaldias  ;i++  )
+            {
+                Console.Clear();
+                Console.WriteLine("Hoy es: {0}", diasSemana.Dequeue());
+                Thread.Sleep(2000);                
+
+            }
+        }
+    }
+}
